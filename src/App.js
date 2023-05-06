997c38d672a5693bffbb353a8328185f45cbfca6
roshan <roshany.bca2020@ssism.org>
2023-05-06 22:50:43 +0530

    iframes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,26 @@ import { Footer } from "./components/Footer";
 function App() {
   return (
     <div className="App">
+      {/* https://clipchamp.com/watch/EuVr8lVh2vz */}
+      {/* <div style={{
+        position: "relative",
+        width: "fit-content",
+        height: "fit-content"
+      }}> */}
+
+      {/* <a style="position:absolute;top:20px;right:1rem;opacity:0.8;" href="https://clipchamp.com/watch/EuVr8lVh2vz?utm_source=embed&utm_medium=embed&utm_campaign=watch"> */}
+      {/* <a style={{
+          position: "absolute",
+          top: "20px",
+          right: "1rem",
+          opacity: "0.8"
+        }} href="https://clipchamp.com/watch/EuVr8lVh2vz?utm_source=embed&utm_medium=embed&utm_campaign=watch">
+          <img style={{ height: "22px" }} src="https://clipchamp.com/e.svg" alt="Made with Clipchamp" />
+        </a>
+        <iframe allow="autoplay;" allowfullscreen style={{
+          border: "none"
+        }} src="https://clipchamp.com/watch/EuVr8lVh2vz/embed" width="640" height="360"></iframe>
+      </div> */}
       <NavBar />
       <Banner />
       <Skills />
