dd9130497f74d75372b640ba435b69dc456efbf8
roshan <roshany.bca2020@ssism.org>
2023-05-12 01:36:07 +0530

    add layout

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,27 +2,27 @@ import React from "react";
 
 const Card = ({ item }) => {
 
-  
+
     return (
-        <div className='_card'>
-            <div className="_card_img">
-                <img src={''} alt="something" />
-            </div>
+        <div className='_card _grid_item'>
             <div className="_card_body">
+                <div className="_card_img">
+                    <img src={''} alt="something" />
+                </div>
                 <p className="_bold">{item.name}</p>
-                <p className="_bold">Mission ids : 
-                <span className="_light">
-                    
-                    {item.mission_id.map((id, index) => {
-                        return <span key={index}>{id}</span>
-                    })}
-                 </span>
+                <p className="_bold">Mission ids :
+                    <span className="_light">
+
+                        {item.mission_id.map((id, index) => {
+                            return <span key={index}>{id}</span>
+                        })}
+                    </span>
                 </p>
                 <p className="_bold">Launch Year : <span className="_light">{item.launch_year}</span></p>
                 <p className="_bold">Successful Launch : <span className="_light">{item.launch_success}</span></p>
                 <p className="_bold">Successful Landing : <span className="_light">{item.launch_success}</span></p>
-                </div>
-            
+            </div>
+
         </div>
     )
 }
--- src/pages/Program.jsx
+++ src/pages/Program.jsx
@@ -1,14 +1,12 @@
-import React, { useState, Suspense, lazy } from 'react'
+import React, { Suspense, lazy } from 'react'
 import { useGetSpaceDataQuery } from '../services/spaceService';
 
 const Card = lazy(() => import('../components/Card.jsx'));
 
-let years = [
-    '2006', '2007', '2008', '2009', '2010', '2011', '2012','2013','2014','2015','2016','2017','2018','2019','2020'
-]
+let years = ['2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020']
 function Program() {
 
-    const { data: task, isSuccess, refetch } = useGetSpaceDataQuery();
+    const { data: task, isSuccess } = useGetSpaceDataQuery();
 
     return (
         <div className="App">
@@ -16,37 +14,44 @@ function Program() {
                 <div className='_header'>
                     <h1>SpaceX Launch Programs</h1>
                 </div>
-                <div className='_grid'>
-                    <div className='_filters'>
+
+                <div className='_flex _flex_wrap'>
+
+                    {/* filter section */}
+                    <div className='_filter_section'>
                         <h3>Filters</h3>
-                        <div className='_filter'>
-                            <p className='_bold _centered'>Launch Year</p>
-                            <div className='_filter_btns'>
-                                <div className='_flex _flex_wrap'>
+                        <p className='_bold _centered'>Launch Year</p>
+                        <div className='_filter_btns'>
+                            <div className='_flex _flex_wrap'>
                                 {years.map((year, index) => {
                                     return <button className='_btn' key={index}>{year}</button>
                                 })}
-                                </div>
-                            </div>
-                            <p className='_bold' > Successful / Launch </p>
-                            <div className='_filter_btns'>
-                                <button className='_btn'>True</button>
-                                <button className='_btn'>False</button>
-                            </div>
-                            <p className='_bold' > Successful / Landing </p>
-                            <div className='_filter_btns'>
-                                <button className='_btn'>True</button>
-                                <button className='_btn'>False</button>
                             </div>
                         </div>
+                        <p className='_bold' > Successful / Launch </p>
+                        <div className='_filter_btns'>
+                            <button className='_btn'>True</button>
+                            <button className='_btn'>False</button>
+                        </div>
+                        <p className='_bold' > Successful / Landing </p>
+                        <div className='_filter_btns'>
+                            <button className='_btn'>True</button>
+                            <button className='_btn'>False</button>
+                        </div>
                     </div>
-                        {isSuccess && task.map((item, index) => {
-                            return (
-                                <Suspense fallback={<div>Loading...</div>} key={index}>
-                                    <Card item={item} />
-                                </Suspense>
-                            )
-                        })}
+
+                    {/* card section */}
+                    <div className='_card_section'>
+                        <div className='_grid'>
+                            {isSuccess && task.map((item, index) => {
+                                return (
+                                    <Suspense fallback={<div>Loading...</div>} key={index}>
+                                        <Card item={item} />
+                                    </Suspense>
+                                )
+                            })}
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
